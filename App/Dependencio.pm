c0d5695e2be60ab3142695d5f39ad0bac72f74e5
Daniel Remeseiro <daniel.remeseiro@gmail.com>
2014-02-08 19:57:53 +0100

    inow works as standalone cli app

--- App/Dependencio.pm
+++ App/Dependencio.pm
@@ -1,32 +1,66 @@
-package Dependencio;
+package App::Dependencio;
+use base qw(App::Cmd::Simple);
 
 use strict;
 use warnings;
 use File::Find;
 use Cwd;
 use IO::File;
+use Term::ANSIColor;
 use Module::Load;
-require Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT = qw(checkDeps);
 our $VERSION = '0.01';
 
 
-sub checkDeps{
-    my $cwd  = getcwd();
 
+sub opt_spec {
+  return (
+    [ "testdirs|t",  "exclude dir named t (tests)" ],
+    [ "verbose|v",  "verbose output"],
+    [ "cpanm|c",  "automatic cpanm install missing modules"],
+    [ "help|h",  "this help menu"],
+  );
+}
+
+
+
+sub validate_args {
+  my ($self, $opt, $args) = @_;
+  $self->usage_error("Bad command") if @$args;
+  $self->usage if $opt->{help};
+}
+
+
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+  our $opts = $opt;
+  $self->checkDeps;
+}
+
+
+
+sub checkDeps{
+    my ($self, $opt) = @_;
+    our $cwd  = getcwd();
     my @dirs = ();
     push (@dirs,$cwd);
 
     find(\&_openFiles, @dirs);
-    return 1;
 }
 
 
+
 sub _openFiles{
+    our $opts;
+    our $cwd;
+    my $dir = $cwd.'/t';
+    my $tests = 1;
+    if( $dir eq $File::Find::dir and $opts->{testdirs}  ){
+       $tests = 0;
+    };
     #only open file types to search module declarations (.pm and .pl)
-    if(-f && m/\.(pm|pl)$/){
-        print  STDOUT "* checking dependecies on $File::Find::name\n";
+    if(-f && m/\.(pm|pl)$/ and $tests == 1){
+        print STDOUT "* checking dependecies on $File::Find::name\n" if $opts->{verbose};
         my $file = $File::Find::name;
         my $fh = IO::File->new($file, O_RDONLY) or die 'I can not open file ', $file, ": $!";
 
@@ -41,7 +75,8 @@ sub _openFiles{
 
                 eval{ load $line };
                 if($@) {
-                    warn "Dependencio says: module $line not found\n";
+                    print STDOUT colored ['bright_red'], "Dependencio says: module $line not found\n";
+                    system "cpanm $line" if $opts->{cpanm};
                 }
             }
         }
---
+++ t/Tests.pm
@@ -0,0 +1,12 @@
+package Tests;
+
+use strict;
+use warnings;
+use Tests::Lol::Cat;
+use Tests::Nyan::Cat;
+use Tests::Super::Powers;
+
+
+
+1;
+
